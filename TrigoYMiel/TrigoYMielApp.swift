a3dfb323c41be1c1dd86df7eb5aac442ce3b7fab
saraascencio <105680448+saraascencio@users.noreply.github.com>
2026-03-31 20:16:23 -0700

    Initial Commit

---
+++ TrigoYMiel/TrigoYMielApp.swift
@@ -0,0 +1,21 @@
+//
+//  TrigoYMielApp.swift
+//  TrigoYMiel
+//
+//  Created by Sara Ascencio on 31/3/26.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct TrigoYMielApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
