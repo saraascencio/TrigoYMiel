45c84469669b4b03cc9cbe1a86e49e1970d31750
saraascencio <105680448+saraascencio@users.noreply.github.com>
2026-04-04 09:08:19 -0700

    Implementación de la capa Data:
    -Repositorios, Data Sources y Mappers
    - Integración de Network y Persistence
    - Configuración de Firebase (Firestore, Auth, Storage)
    - Agregado de recursos visuales (colores y logo)
    - Pruebas con Login y Registro funcionales

---
+++ TrigoYMiel/Data/Persistence/CoreDataEntities/CDProductCache+CoreDataClass.swift
@@ -0,0 +1,17 @@
+//
+//  CDProductCache+CoreDataClass.swift
+//  TrigoYMiel
+//
+//  Created by Sara Ascencio on 4/4/26.
+//
+//
+
+public import Foundation
+public import CoreData
+
+public typealias CDProductCacheCoreDataClassSet = NSSet
+
+
+public class CDProductCache: NSManagedObject {
+
+}
